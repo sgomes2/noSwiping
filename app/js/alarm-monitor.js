a35879b79735e23ed17eb6ee556c875092c6f76e
Saulo Gomes <saulogomes3@gmail.com>
2023-03-10 22:05:59 -0500

    adding logic to play audio

--- app/js/alarm-monitor.js
+++ app/js/alarm-monitor.js
@@ -9,9 +9,9 @@ var audio = new Audio(`${__dirname}/../assets/audio/screaming.mp3`);
 audio.loop = true;
 
 // Keep track of battery charging status
-let batteryIsCharging = true;
+let batteryIsCharging;
 //  Keep track of alarm activation status
-let activated = true;
+let activated = false;
 
 var audio = new Audio(`${__dirname}/../assets/audio/screaming.mp3`);
 
@@ -20,15 +20,23 @@ document.getElementById("activate-button").addEventListener("click", () => {
   activated = true;
 });
 
-// navigator.getBattery().then((battery) => {
-//   console.log("Battery Charging: " + batteryIsCharging);
-//   batteryIsCharging = battery.charging;
+navigator.getBattery().then((battery) => {
+  console.log("Battery Charging: " + batteryIsCharging);
+  batteryIsCharging = battery.charging;
+  setActivateInfo();
 
-//   battery.addEventListener("chargingchange", () => {
-//     batteryIsCharging = battery.charging;
-//     console.log("Battery Charging: " + batteryIsCharging);
-//   });
-// });
+  battery.addEventListener("chargingchange", () => {
+    batteryIsCharging = battery.charging;
+    console.log("Battery Charging: " + batteryIsCharging);
+
+    if (activated && !batteryIsCharging) {
+      audio.play();
+    } else if (activated && batteryIsCharging) {
+      audio.pause();
+      audio.currentTime = 0;
+    }
+  });
+});
 
 const setActivateInfo = () => {
   const activateButton = document.getElementById("activate-button");
